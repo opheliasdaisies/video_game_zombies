9850b0f0673430f39fb3226d505eebdd52910e06
Sara Gorecki <sgorecki@gmail.com>
2014-02-09 22:58:17 -0500

    add e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -20,6 +20,20 @@ describe('PhoneCat App', function() {
       expect(repeater(".phones li").count()).toBe(2);
     });
 
+    it("should be possible to control phone order via the drop down select box", function() {
+      input("query").enter("tablet");
+ 
+      expect(repeater(".phones li", "Phone List").column("phone.name")).
+          toEqual(["Motorola XOOM\u2122 with Wi-Fi",
+                   "MOTOROLA XOOM\u2122"]);
+ 
+      select("orderProp").option("Alphabetical");
+ 
+      expect(repeater(".phones li", "Phone List").column("phone.name")).
+          toEqual(["MOTOROLA XOOM\u2122",
+                   "Motorola XOOM\u2122 with Wi-Fi"]);
+    });
+
   });
 
 });
