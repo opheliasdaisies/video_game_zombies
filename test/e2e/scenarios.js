901158cb0740d8ae2450a892e4c91bf9cde56c49
Sara Gorecki <sgorecki@gmail.com>
2014-02-10 00:07:58 -0500

    fix e2e test to account for external data, and add test to check for phone-specific links

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -11,13 +11,13 @@ describe('PhoneCat App', function() {
     });
 
     it("should filter the phone list as user types into the search box", function(){
-      expect(repeater(".phones li").count()).toBe(3);
+      expect(repeater(".phones li").count()).toBe(20);
 
       input("query").enter("nexus");
       expect(repeater(".phones li").count()).toBe(1);
 
       input("query").enter("motorola");
-      expect(repeater(".phones li").count()).toBe(2);
+      expect(repeater(".phones li").count()).toBe(8);
     });
 
     it("should be possible to control phone order via the drop down select box", function() {
@@ -32,6 +32,12 @@ describe('PhoneCat App', function() {
       expect(repeater(".phones li", "Phone List").column("phone.name")).
           toEqual(["MOTOROLA XOOM\u2122",
                    "Motorola XOOM\u2122 with Wi-Fi"]);
+    });
+
+    it("should render phone-specific links", function(){
+      input("query").enter("nexus");
+      element(".phones li a").click();
+      expect(browser().location().url()).toBe("/phones/nexus-s");
     });
 
   });
